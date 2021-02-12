7aac72dbd94521ce69cd2c20148fe8e3e80ff2cb
Vasiliy Pankrukhin <wacpan93@gmail.com>
2021-02-12 16:43:00 +0300

    Add project
    Копирование проекта из Visual Studio в репозиторий

---
+++ Lessons/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя пользователя:");
+            var user_name = Console.ReadLine();
+            Console.WriteLine($"Привет, {user_name}, сегодня {DateTime.Now:d}");
+            Console.ReadLine();
+        }
+    }
+}
